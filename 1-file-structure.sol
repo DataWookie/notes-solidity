eee2d9ef0f1be7892dc6289f68b2f3d0b7340328
Andrew B. Collier <andrew@exegetic.biz>
2017-12-22 07:41:22 +0200

    Added example of file structure.

---
+++ 1-file-structure.sol
@@ -0,0 +1,50 @@
+pragma solidity ^0.4.18;
+
+// Import any files that are dependencies.
+//
+import "dependency-file.sol";
+
+contract BaseContract {
+    // State variables
+    //
+    address creator;
+    mapping (address => uint) public balances;
+ 
+    // Constructor
+    //
+    // - has same name as contract
+    // - can have arguments
+    // - used to initialise state variables
+    //
+    function ExampleContract() public {
+        creator = msg.sender;
+    }
+
+    modifier onlyCreator() {
+        require(msg.sender == creator);
+        _;
+    }
+
+    function kill() public onlyCreator { 
+        selfdestruct(creator);  
+    }
+
+}
+
+contract DerivedContract is BaseContract {
+    uint public counter = 0;
+
+    // Events
+    //
+    event Action(uint _number);
+
+    function DerivedContract() public {
+    }
+
+    // @notice Perform some action
+    // @param _number A number
+    // @return Whether the action was successful or not
+    function action(uint _number) public returns (bool) {
+        Action(_number);
+    }
+}
